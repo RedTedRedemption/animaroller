159fab4e74a8bf92e22ecd2c24a14fba5bb2b335
RedTedRedemption <teddy.herzfeld@icloud.com>
2017-10-13 22:36:25 -0400

    starting algorithm

--- src/Armor.java
+++ src/Armor.java
@@ -27,6 +27,8 @@ public class Armor {
     public int quality;
     public String comments_and_additional_effects;
 
+    public int[] defenses = new int[7];
+
     public Armor(int ArmorType_cut, int ArmorType_impact, int ArmorType_thrust, int ArmorType_heat, int ArmorType_electricity, int ArmorType_cold, int ArmorType_energy, int ArmorRequirement, int NaturalPenalty, int Initiative, int Presence, int Fortitude, int MovementPenalty, int Quality) {
 
         armorType_cut = ArmorType_cut + (quality / 5);
@@ -36,6 +38,15 @@ public class Armor {
         armorType_electricity = ArmorType_electricity + (quality / 5);
         armorType_cold = ArmorType_cold + (quality / 5);
         armorType_energy = ArmorType_energy + (quality / 5);
+
+        defenses[main.ARMOR_DAMAGE_TYPE_CUT] = armorType_cut;
+        defenses[main.ARMOR_DAMAGE_TYPE_IMPACT] = armorType_impact;
+        defenses[main.ARMOR_DAMAGE_TYPE_THRUST] = armorType_thrust;
+        defenses[main.ARMOR_DAMAGE_TYPE_HEAT] = armorType_heat;
+        defenses[main.ARMOR_DAMAGE_TYPE_ELECTRICITY] = armorType_electricity;
+        defenses[main.ARMOR_DAMAGE_TYPE_COLD] = armorType_cold;
+        defenses[main.ARMOR_DAMAGE_TYPE_ENERGY] = armorType_energy;
+
         armorRequirement = ArmorRequirement;
         naturalPenalty = NaturalPenalty;
         initiative = Initiative;
@@ -58,6 +69,7 @@ public class Armor {
     public Armor(int ArmorType, int ArmorRequirement, int NaturalPenalty, int Initiative, int Presence, int Fortitude, int MovementPenalty, int Quality, String Comments) {
 
 
+
         armorRequirement = ArmorRequirement;
         naturalPenalty = NaturalPenalty;
         initiative = Initiative;
@@ -76,6 +88,10 @@ public class Armor {
 
 
 
+    }
+
+    public int getDefenceByType(int damageType) {
+        return defenses[damageType];
     }
 
 
--- src/main.java
+++ src/main.java
@@ -14,6 +14,8 @@ public class main {
     public static final int ARMOR_DAMAGE_TYPE_ENERGY = 6;
     public static final int ARMOR_DAMAGE_TYPE_NONE = 7;
 
+
+
     static Scanner scanner = new Scanner(System.in);
 
     public static Character attacker = new Character(130, 90, 0, 90, 5, 0, 7, 40, 50, 50, 50, 40, 35, 130);
@@ -23,25 +25,97 @@ public class main {
     public static Armor attacker_armor = new Armor(1, 0, 2, 1, 2, 1, 0, 10, 0, 0, 25, 12, 1, 0);
     public static Armor longcoat = new Armor(1, 0, 2, 1, 2, 2, 0, 0, -5, 0, 25, 10, 0, 5);
 
+    static Weapon attacking_weapon = falchion;
+    static Armor defender_armor = longcoat;
+    static Character defender = jesuskun;
+    static Weapon defender_weapon = katana;
+    static Armor attackerArmor = attacker_armor;
+    static int counterAttackModifier;
+    static int finalDefense;
+    static boolean reportHit;
+    static int counterAttackRoll;
+    static int counterDefendRoll;
+    static int finalCounterAttack;
+    static int finalCounterDefense;
+    static int counterCombatResult;
+    static int combatResult;
+    static int finalAttack;
 
     //IF COMBAT SITUATION ATT == NULL, PROBABLY MEANS NOT POSSIBLE OR SOME SHIT
 
     public static void main(String[] args) {
-        Weapon attacking_weapon = falchion;
-        Armor defender_armor = longcoat;
-        Character defender = jesuskun;
+
+
         System.out.print("attack roll: ");
         int attackRoll = scanner.nextInt();
         System.out.print("defend roll: ");
         int defendRoll = scanner.nextInt();
 
-        int finalAttack = attacker.static_attack + attackRoll;
+        finalAttack = attacker.static_attack + attackRoll;
 
-        int finalDefense = jesuskun.static_dodge + defendRoll;
+        System.out.print("dodge or block? ");
+        boolean choseDodge = scanner.next().toLowerCase().contains("dodge");
 
-        int combatResult = finalAttack - finalDefense;
 
-        boolean reportHit;
+        if (choseDodge) {
+            finalDefense = defender.static_dodge + defendRoll;
+            System.out.print("final defense: ");
+            System.out.println(finalDefense);
+        } else {
+            finalDefense = defender.static_block + defendRoll;
+            System.out.print("final defense: ");
+            System.out.println(finalDefense);
+        }
+
+        combatResult = finalAttack - finalDefense;
+        System.out.print("combatResult: ");
+        System.out.println(combatResult);
+
+        //determine if we should counterattack
+
+        if (combatResult < 0) {
+            //counterattack
+            System.out.println("counterattack: SURPRISE MOTHERFUCKA!!!");
+
+            counterAttackModifier =  roundDownTen(Math.abs(combatResult) / 10) * 5;
+
+            System.out.print("counterAttackModifier ");
+            System.out.println(counterAttackModifier);
+
+            System.out.print("dodge or block? ");
+            choseDodge = scanner.next().contains("dodge");
+
+            System.out.print("roll again for defender's counterattack: ");
+            counterAttackRoll = scanner.nextInt();
+
+            finalCounterAttack = counterAttackRoll + defender.static_attack + counterAttackModifier;
+
+            System.out.print("roll again for attacker's defense against the counterattack: ");
+            counterDefendRoll = scanner.nextInt();
+
+            if (choseDodge) {
+                finalCounterDefense = counterDefendRoll + attacker.static_dodge;
+            } else {
+                finalCounterDefense = counterDefendRoll + attacker.static_block;
+            }
+
+            counterCombatResult = finalCounterAttack - finalCounterDefense;
+
+            if (counterCombatResult < 0) {
+                System.out.println("YOU FAIL BITCH!");
+            } else if (counterCombatResult < 30) {
+                reportHit = true;
+                System.out.println("you managed to hit them, but didn't do any damage");
+                System.out.println("YOU FAIL BITCH!...slightly less");
+            } else {
+                System.out.print("deal ");
+                System.out.println(calculateDamage(defender, attacker, defender_weapon, attacker_armor, counterCombatResult));
+            }
+
+
+
+
+        }
 
         boolean counterAttack = false;
 
@@ -62,23 +136,23 @@ public class main {
             int damage = weapon_damage * combatResult;
         }
 
-
-
-        if (combatResult < 0) {
-            int counterAttackModifier = Math.abs(combatResult) / 10;
-            counterAttackModifier = (counterAttack / 10) * 10;
-            counterAttackModifier = counterAttackModifier * 5;
-            System.out.print("roll again for counter: ");
-            int defAttackRoll = scanner.nextInt();
-            finalAttack = defAttackRoll + defender.static_attack + counterAttackModifier;
-            System.out.print("and again for attacker (now defender): ");
-            int attackerReroll = scanner.nextInt();
-            finalDefense = attackerReroll + attacker.static_block;
-            combatResult = finalAttack - finalDefense;
-            System.out.println(combatResult);
-        }
-
-        System.out.println();
+//
+//
+//        if (combatResult < 0) {
+//            int counterAttackModifier = Math.abs(combatResult) / 10;
+//            counterAttackModifier = (counterAttack / 10) * 10;
+//            counterAttackModifier = counterAttackModifier * 5;
+//            System.out.print("roll again for counter: ");
+//            int defAttackRoll = scanner.nextInt();
+//            finalAttack = defAttackRoll + defender.static_attack + counterAttackModifier;
+//            System.out.print("and again for attacker (now defender): ");
+//            int attackerReroll = scanner.nextInt();
+//            finalDefense = attackerReroll + attacker.static_block;
+//            combatResult = finalAttack - finalDefense;
+//            System.out.println(combatResult);
+//        }
+//
+//        System.out.println();
 
 
 
@@ -96,6 +170,24 @@ public class main {
         }
         return 0;
     }
+
+    public static int roundDownTen(int val) {
+        String valString = Integer.toString(val);
+        char[] chars = valString.toCharArray();
+        chars[chars.length - 1] = '0';
+        return Integer.parseInt(valString);
+    }
+
+    public static int calculateDamage(Character Attacker, Character Defender, Weapon attackWeapon, Armor defendArmor, int CombatResult) {
+        CombatResult = roundDownTen(CombatResult - (10 * defendArmor.getDefenceByType(attackWeapon.attackType_primary)));
+        System.out.println(combatResult);
+        if (CombatResult < 0) {
+            CombatResult = 0;
+        }
+        int damageModifier = combatResult;
+        return ((int) (((double) damageModifier) * (attackWeapon.baseDamage + Attacker.static_strength)));
+    }
+
 
 
 
